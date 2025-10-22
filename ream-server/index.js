632d87826785c7f2e8cd1f09c729e581fcc86a41
A-Vandegriff <austinvandegriff@yahoo.com>
2025-10-21 23:58:13 -0400

    created auth, subscription, user routes

--- ream-server/index.js
+++ ream-server/index.js
@@ -3,7 +3,18 @@ import  express from 'express';
 import  cors from 'cors';
 import  router  from './routes/articles.js'; //importing the router articles.js
 
+import  userRouter  from './routes/user,routes.js';
+import  subscriptionRouter  from './routes/subscription.routes.js';
+import  authRouter  from './routes/auth.routes.js';
+
+
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
 
 
 app.use(cors());
